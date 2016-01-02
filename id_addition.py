a2228ab7a167228456904d66cb84c2b49b8fdf05
Samweli <smwltwesa6@gmail.com>
2016-01-02 11:04:09 +0300

    created abstract script

---
+++ id_addition.py
@@ -0,0 +1,28 @@
+import sqlite3
+import csv
+
+
+def add(){
+	connection = connect('database/duplicates_to_identify')
+	c = connection.cursor()
+
+	with open('DISTRICT-COUNCIL-ROADNETWORK-Cleaned-csv_.csv') as csvfile:
+
+		reader = csv.DictReader(csvfile)
+		for row in reader:
+			# TODO take road name and id
+			id = row['ID Number']
+			road_name = row['Road Name']
+
+			t = (road_name)
+			c.execute('SELECT * FROM  WHERE Road Name LIKE ?', t)
+			c.fetchone[0]
+
+
+}
+
+def connect(name){
+	conn = sqlite3.connect(name)
+
+	return conn
+}
